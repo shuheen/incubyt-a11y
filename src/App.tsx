0ee543090f9545a11d9e7fd126782e7bfb0f90d0
Shoaib <mail.shuaibkhan@gmail.com>
2025-10-26 08:07:43 +0530

    Basic a11y issues fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,41 +7,44 @@ const App = () => {
   const handleCalculate = () => {};
 
   return (
-    <div style={{ padding: '20px', backgroundColor: '#fff', color: '#aaa' }}>
+    <main style={{ padding: '20px', backgroundColor: '#fff', color: '#aaa' }}>
       <img
         src='https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         width={600}
+        alt='String Calculator'
         height={400}
       />
 
-      <h2>String Calculator</h2>
+      <h1>String Calculator</h1>
 
-      <h1 style={{ fontSize: '20px' }}>Enter numbers</h1>
+      <h2 style={{ fontSize: '20px' }}>Enter numbers</h2>
 
       <textarea
         style={{ margin: '10px 0', color: '#aaa' }}
         placeholder='Enter numbers'
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        name='numbers'
       />
 
-      <div
+      <button
         onClick={handleCalculate}
         style={{
           padding: '10px',
           backgroundColor: '#008cba',
           color: '#fff',
           border: 'none',
+          display: 'block',
         }}>
         Calculate
-      </div>
+      </button>
 
       {result !== null && <p style={{ color: 'green' }}>Result: {result}</p>}
 
       <div role='alert'>
         <p>Make sure you enter numbers correctly!</p>
       </div>
-    </div>
+    </main>
   );
 };
 
